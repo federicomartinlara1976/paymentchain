701baa37222df14c4497a35da8089b50b3dc40d4
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-22 12:23:06 +0200

    Add web client

--- businessdomain/customer/src/main/java/net/bounceme/chronos/paymentchain/customer/controller/CustomerController.java
+++ businessdomain/customer/src/main/java/net/bounceme/chronos/paymentchain/customer/controller/CustomerController.java
@@ -4,7 +4,9 @@
  */
 package net.bounceme.chronos.paymentchain.customer.controller;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +18,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import net.bounceme.chronos.paymentchain.customer.dto.CustomerDTO;
 import net.bounceme.chronos.paymentchain.customer.service.CustomerService;
+import reactor.netty.http.client.HttpClient;
 
 /**
  *
@@ -30,6 +39,28 @@ public class CustomerController {
     
     @Autowired
     private CustomerService customerService;
+    
+    private final WebClient.Builder webClientBuilder;
+
+	public CustomerController(Builder webClientBuilder) {
+		this.webClientBuilder = webClientBuilder;
+	}
+	
+	//webClient requires HttpClient library to work propertly       
+    HttpClient client = HttpClient.create()
+            //Connection Timeout: is a period within which a connection between a client and a server must be established
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+            .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+            //Response Timeout: The maximun time we wait to receive a response after sending a request
+            .responseTimeout(Duration.ofSeconds(1))
+            // Read and Write Timeout: A read timeout occurs when no data was read within a certain 
+            //period of time, while the write timeout when a write operation cannot finish at a specific time
+            .doOnConnected(connection -> {
+                connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+            });
 
 	@GetMapping()
     public ResponseEntity<List<CustomerDTO>> list() {
