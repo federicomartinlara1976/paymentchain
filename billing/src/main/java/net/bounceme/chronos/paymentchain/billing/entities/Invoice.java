02dedf930fd9ea588ca81b2268d3bf163fe26fa0
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-09-11 19:57:55 +0200

    Documentación de campos para swagger

--- billing/src/main/java/net/bounceme/chronos/paymentchain/billing/entities/Invoice.java
+++ billing/src/main/java/net/bounceme/chronos/paymentchain/billing/entities/Invoice.java
@@ -7,6 +7,8 @@ package net.bounceme.chronos.paymentchain.billing.entities;
 
 import java.io.Serializable;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder(toBuilder = true)
 @Entity
+@Schema(name = "Invoice", description = "Model represent a invoice on database")
 public class Invoice implements Serializable {
 	
 	/**
@@ -36,8 +39,10 @@ public class Invoice implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@Schema(name = "customerId", requiredMode = RequiredMode.REQUIRED, example = "2", defaultValue = "1", description = "Unique id of customer that represent the owner of invoice")
 	private Long customerId;
 	
+	@Schema(name = "number", requiredMode = RequiredMode.REQUIRED, example = "3", defaultValue = "8", description = "Number given on fisical invoice")
 	private String number;
 	
 	private String detail;
