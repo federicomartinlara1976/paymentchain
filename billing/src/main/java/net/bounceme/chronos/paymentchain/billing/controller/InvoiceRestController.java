cec3b515291c10b39c7ee34d3cca03f5cd88a705
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-09-11 20:57:05 +0200

    Anotaciones de documentación para swagger

--- billing/src/main/java/net/bounceme/chronos/paymentchain/billing/controller/InvoiceRestController.java
+++ billing/src/main/java/net/bounceme/chronos/paymentchain/billing/controller/InvoiceRestController.java
@@ -5,28 +5,33 @@
  */
 package net.bounceme.chronos.paymentchain.billing.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.bounceme.chronos.paymentchain.billing.entities.Invoice;
 import net.bounceme.chronos.paymentchain.billing.repository.InvoiceRepository;
-
-import java.util.Optional;
-import org.springframework.http.HttpStatus;
 
 /**
  *
  * @author sotobotero
  */
+@Tag(name = "Billing API", description = "This API serve all funcionality for management invoices")
 @RestController
 @RequestMapping("/billing")
 public class InvoiceRestController {
@@ -34,6 +39,9 @@ public class InvoiceRestController {
     @Autowired
     InvoiceRepository billingRepository;
     
+    @Operation(description = "Return all invoices bundled into Response", summary = "Return 204 if no data found")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK"),
+    		@ApiResponse(responseCode = "500", description = "Internal error")})
     @GetMapping()
     public List<Invoice> list() {
         return billingRepository.findAll();
