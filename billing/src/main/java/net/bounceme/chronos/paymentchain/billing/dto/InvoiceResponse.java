22aceea94d1e86f9b6bc5b2cccad650658769f90
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-09-15 20:07:56 +0200

    Se crean objetos DTO de request y response para separación de capas

---
+++ billing/src/main/java/net/bounceme/chronos/paymentchain/billing/dto/InvoiceResponse.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.bounceme.chronos.paymentchain.billing.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author sotobotero
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Schema(name = "InvoiceResponse", description = "Model represent a invoice on database")
+public class InvoiceResponse {
+	
+	private Long id;
+	
+	@Schema(name = "customerId", requiredMode = RequiredMode.REQUIRED, example = "2", defaultValue = "1", description = "Unique id of customer that represent the owner of invoice")
+	private Long customerId;
+	
+	@Schema(name = "number", requiredMode = RequiredMode.REQUIRED, example = "3", defaultValue = "8", description = "Number given on fisical invoice")
+	private String number;
+	
+	private String detail;
+	
+	private Double amount;
+}
